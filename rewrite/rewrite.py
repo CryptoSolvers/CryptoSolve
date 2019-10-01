160b16b1c4f8f42c236767d89ef5a2385efcb2e1
Brandon Rozek <rozekbrandon@gmail.com>
2019-09-30 20:45:53 -0400

    Appropriately manage overlaping variable names and matching algorithm. Also allow easy creation of the converse.

--- rewrite/rewrite.py
+++ rewrite/rewrite.py
@@ -3,6 +3,19 @@ sys.path.append("..")
 
 from algebra import *
 from Unification import unif
+from copy import deepcopy
+
+# Turn Variables into Constants
+def freeze(term : Term):
+    term = deepcopy(term)
+    if isinstance(term, Variable):
+        return Constant(term.symbol)
+    elif isinstance(term, FuncTerm):
+        term.arguments = list(term.arguments)
+        for i,t in enumerate(term.arguments):
+            term.arguments[i] = freeze(t)
+        term.arguments = tuple(term.arguments)
+    return term
 
 class RewriteRule:
     def __init__(self, hypothesis : Term, conclusion : Term):
@@ -13,22 +26,40 @@ class RewriteRule:
         # Change common variables in RewriteRule if they exist
         overlaping_vars = self._getOverlapVars(term)
         while overlaping_vars:
-            self._changeVars(overlaping_vars)
+            self._changeVars(overlaping_vars, term)
             overlaping_vars = self._getOverlapVars(term)
         # Perform matching and substitution
-        sigma = unif(self.hypothesis, term)
-        return self.conclusion * sigma if sigma else term
+        frozen_term = freeze(term)
+        sigma = unif(self.hypothesis, frozen_term)
+        return self.conclusion * sigma if sigma else deepcopy(term)
     def __repr__(self):
         return str(self.hypothesis) + " → " + str(self.conclusion)
     def _getOverlapVars(self, term):
         rewrite_vars = get_vars(self.hypothesis, unique = True) | get_vars(self.conclusion, unique = True)
         term_vars = get_vars(term, unique = True)
-        return rewrite_vars & term_vars
-    def _changeVars(self, overlaping_vars):
-        new_vars = map(lambda v: Variable(v.symbol + "_1"), overlaping_vars)
+        return list(rewrite_vars & term_vars)
+    def _changeVars(self, overlaping_vars, term):
+        all_vars = get_vars(term, unique = True) | get_vars(self.hypothesis, unique = True) | get_vars(self.conclusion, unique = True)
+        new_vars = []
+        # Go through all the variables that share the same symbol between the term and rewrite rule
+        # and change the variables in the rewrite rule
+        for v in overlaping_vars:
+            new_var = v
+            # Keep renaming variable in rewrite rule until it is not an already existing variable
+            while new_var in all_vars:
+                new_var = Variable(new_var.symbol + "_1")
+            new_vars.append(new_var)
+        # Create substitution between the old and new variable names and apply them
         s = SubstituteTerm()
         for old_v, new_v in zip(overlaping_vars, new_vars):
             s.add(old_v, new_v)
         self.hypothesis *= s
         self.conclusion *= s
-        
+
+def converse(rule : RewriteRule):
+    new_rule = deepcopy(rule)
+    # Flip Hypothesis and Conclusion
+    temp = new_rule.hypothesis
+    new_rule.hypothesis = new_rule.conclusion
+    new_rule.conclusion = temp
+    return new_rule
