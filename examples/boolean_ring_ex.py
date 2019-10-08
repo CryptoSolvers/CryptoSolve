ebbe6fd5d06282bd0b9723332638ab1aa19e7722
Brandon Rozek <rozekbrandon@gmail.com>
2019-10-08 11:58:22 -0400

    Added an example file for boolean rings

---
+++ examples/boolean_ring_ex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append("..")
+
+from algebra import *
+from theories import *
+
+a = BooleanRingElement("a")
+b = BooleanRingElement("b")
+zero = BoolRingZero()
+
+print("a ⊕ a =", str(a + a))
+print("a ⊗ a =", str(a * a))
+print("a ⊕ 0 =", str(a + zero))
+print("a ⊗ 0 =", str(a * zero))
+print("a ⊗ b = b ⊗ a is", a * b == b * a)
+print("a ⊕ b = b ⊕ a is", a + b == b + a)
+print("a ⊕ a ⊕ b =", str(a + a + b))
+print("a ⊕ b =", str(a + b))
+print("a ⊗ 0 =", str(a * zero))
+print("b ⊗ (a ⊕ a) =", str(b * (a + a)))
+print("a ⊗ b =", str(a * b))
