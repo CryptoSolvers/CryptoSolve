984ec29483014e70cd050f41c512d3d44482846a
Brandon Rozek <rozekbrandon@gmail.com>
2019-10-09 17:28:43 -0400

    Added commented version of distributivity rules. Need to check syntactic unification to see if it's taking a while to find it or if it's not working

--- theories/rings.py
+++ theories/rings.py
@@ -2,6 +2,7 @@ from algebra import *
 from .ac import *
 from .groups import *
 from copy import deepcopy
+from rewrite import *
 
 class Ring:
     def __init__(self, name : str, add_operation : ACFunction, mul_operation : AFunction, zero_symbol = "0"):
@@ -27,11 +28,30 @@ class RingElement(GroupElement):
         return super().__mul__(x)
     def __radd__(self, x):
         return super().__rmul__(x)
+    # Properties of multiplication return (True, result) if one matches otherwise (false, None)
+    def _mulprops(self, x):
+        if x == self.ring.zero or self == self.ring.zero:
+            return (True, deepcopy(self.ring.zero))
+        # # Setting up variables for distribution rules
+        # w = Variable("w")
+        # y = Variable("y")
+        # z = Variable("z")
+        # mul = Function(self.ring.mul.symbol, self.ring.mul.arity)
+        # add = Function(self.ring.add.symbol, self.ring.add.arity)
+        # # Left Distributivity
+        # r = RewriteRule(mul(w, add(y, z)), add(mul(w, y), mul(w, z)))
+        # new_x = r.apply(x)
+        # if new_x is not x:
+        #     return (True, new_x)
+        # # Right Distributivity
+        # r = RewriteRule(mul(add(y, z), w), add(mul(y, w), mul(z, w)))
+        # new_x = r.apply(x)
+        # if new_x is not x:
+        #     return (True, new_x)
+        return (False, None)
     def __mul__(self, x):
-        if x == self.ring.zero or self == self.ring.zero:
-            return deepcopy(self.ring.zero)
-        return self.ring.mul(self, x)
+        matched, term = self._mulprops(x)
+        return self.ring.mul(self, x) if not matched else term
     def __rmul__(self, x):
-        if x == self.ring.zero or self == self.ring.zero:
-            return deepcopy(self.ring.zero)
-        return self.ring.mul(x, self)
+        matched, term = self._mulprops(x)
+        return self.ring.mul(x, self) if not matched else term
