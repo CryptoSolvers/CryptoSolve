289971419e7407c5ed6dd6d78704fa742e6578a6
Brandon Rozek <rozekbrandon@gmail.com>
2019-10-13 23:32:59 -0400

    Example files for theories module

--- examples/theories/boolean_ring_ex.py
+++ examples/theories/boolean_ring_ex.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import sys
-sys.path.append("..")
+sys.path.append("../..")
 
 from algebra import *
 from theories import *
 
-a = BooleanRingElement("a")
-b = BooleanRingElement("b")
+a = RingConstant(BooleanRing, "a")
+b = RingConstant(BooleanRing, "b")
 zero = BooleanRing.zero
 
 print("a ⊕ a =", str(a + a))
@@ -16,7 +16,5 @@ print("a ⊗ 0 =", str(a * zero))
 print("a ⊗ b = b ⊗ a is", a * b == b * a)
 print("a ⊕ b = b ⊕ a is", a + b == b + a)
 print("a ⊕ a ⊕ b =", str(a + a + b))
-print("a ⊕ b =", str(a + b))
-print("a ⊗ 0 =", str(a * zero))
 print("b ⊗ (a ⊕ a) =", str(b * (a + a)))
 print("a ⊗ b =", str(a * b))
