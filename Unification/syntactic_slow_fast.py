3f6e7b34e3769f8230c7b856ff6770c02635027c
Brandon Rozek <rozekbrandon@gmail.com>
2019-10-23 11:55:53 -0400

    Playing around with speeding up the average runtime of syntactic unification

---
+++ Unification/syntactic_slow_fast.py
@@ -0,0 +1,85 @@
+#Slow-Fast syntactic unification
+
+#!/usr/bin/env python3
+from algebra import *
+
+def decomposable(term : Term):
+	return isinstance(term, FuncTerm) and term.function.arity > 0
+
+def syntactic_unif(l: Term, r: Term):
+	U = [Equation(l, r)]
+	solve = set()
+	while bool(U):
+		equations_to_remove = []
+		apply_sigma = False
+		sigma = SubstituteTerm()
+		decomposed_equations = []
+		for i, e in enumerate(U):
+			#Trival
+			if e.left_side == e.right_side:
+				equations_to_remove.append(i)
+				continue # Don't need to perform other checks...
+				
+			#Symbol Clash
+			if isinstance(e.left_side, FuncTerm) and isinstance(e.right_side, FuncTerm):
+				if e.left_side.function.symbol != e.right_side.function.symbol:
+					print('Symbol Clash')
+					return False #Fix the return false
+			
+			#Orient
+			if (isinstance(e.right_side, Variable) and isinstance(e.left_side, FuncTerm)):
+				temp = U[i].right_side
+				U[i].right_side = U[i].left_side
+				U[i].left_side = temp
+			
+			#Occurs Check
+			if isinstance(e.left_side, Variable) and isinstance(e.right_side, FuncTerm):
+				if e.left_side in e.right_side:
+					print('Occurs Check')
+					return False #We could think of an improved method for errors
+			
+			#Decomposition
+			if decomposable(e.left_side) and decomposable(e.right_side):
+				dec = map(lambda t1, t2: Equation(t1, t2), list(e.left_side.arguments), list(e.right_side.arguments))
+				equations_to_remove.append(i)
+				decomposed_equations += dec
+				continue # Variable Elimination can't happen if equation was decomposable
+				
+			#Variable Elimination (passed occurs check at this point)
+			if isinstance(e.left_side, Variable):
+				equations_to_remove.append(i)
+				sigma.add(e.left_side, e.right_side)
+				apply_sigma = True
+				break # Break out of loop so that substitutions can be applied to U
+		#End For
+
+		# Remove solved or decomposed equations
+		# Reverse so that we don't have to pay attention to the order we delete
+		for i in sorted(equations_to_remove, reverse = True):
+			del U[i]
+		
+		# Add new decomposed equations to U
+		U += decomposed_equations
+
+		# Apply substitutions
+		if apply_sigma:
+			# Apply substitutions to U
+			for i, e in enumerate(U):
+				U[i].left_side *= sigma
+				U[i].right_side *= sigma
+			# Apply substitutions to solve set
+			solve = list(solve)
+			for i, e in enumerate(solve):
+				solve[i].left_side *= sigma
+				solve[i].right_side *= sigma
+			solve = set(solve)
+			# Add equation from substitution to solve
+			solve |= {Equation(*(sigma.subs.pop()))}
+
+	
+	delta = SubstituteTerm()	
+	for e in solve:
+		delta.add(e.left_side, e.right_side)
+	print(delta)								
+	return delta
+										
