be9c312404754d29e232a95a2cae0cb7129309c5
Brandon Rozek <rozekbrandon@gmail.com>
2019-10-24 10:33:24 -0400

    Renamed PRR to MOE and updated examples

--- examples/moe_ex.py
+++ examples/moe_ex.py
@@ -2,17 +2,17 @@
 import sys
 sys.path.append("..")
 
-from prr import *
+from moe import *
 
 # Setting up terms
 s = Constant("s")
 x = Variable("x")
 
 print("One interaction with oracle example")
-p = PRR(PropogatingCBC)
+p = MOE(PropogatingCBC)
 p.rcv_start(1)
 print(p.rcv_block(1, x))
 
 # Simulate 10 interactions
 print("Simulating a 10 frame interaction with an oracle")
-print(PRRInteraction(PropogatingCBC, 10))
+print(MOEInteraction(PropogatingCBC, 10))
