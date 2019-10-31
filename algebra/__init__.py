91ca91dfc386894d1bb7e3c68f475e360100dce6
Brandon Rozek <rozekbrandon@gmail.com>
2019-10-31 11:41:42 -0400

    Added string parser to make terms from strings
    Signed-off-by: Brandon Rozek <rozekbrandon@gmail.com>

--- algebra/__init__.py
+++ algebra/__init__.py
@@ -2,3 +2,4 @@ from .term import *
 from .dag import *
 from .substitute import *
 from .sympy import *
+from .parser import *
---
+++ examples/parser_ex.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import sys
+sys.path.append("..")
+
+from algebra import *
+f = Function("f", 2)
+a = Constant("a")
+x = Variable("x")
+p = Parser()
+p.add(f)
+p.add(x)
+p.add(a)
+print("Parsing f(x,a) is successful?", p.parse("f(x,a)") == f(x,a))
+print("Parsing f(f(x,a),f(x,a)) is successful?", p.parse("f(f(x,a),f(x,a))") == f(f(x,a), f(x,a)))
