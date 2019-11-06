7a51c0aa167a3d25d6266066a54727498c5d1dbd
Brandon Rozek <rozekbrandon@gmail.com>
2019-11-06 12:51:31 -0500

    Keep track of substitutions now

--- rewrite/variants.py
+++ rewrite/variants.py
@@ -1,24 +1,26 @@
 from .rewrite import *
 
 
-def narrow(term : Term, goal_term : Term, rules : List[RewriteRule], bound : int, substitions : List[SubstituteTerm] = []) -> List[SubstituteTerm]:
+def narrow(term : Term, goal_term : Term, rules : List[RewriteRule], bound : int, substition : SubstituteTerm = SubstituteTerm()) -> Union[Literal[False], SubstituteTerm]:
     if term == goal_term:
-        return substitions
+        return substition
     if bound < 1:
         return False
     for rule in rules:
         sub, new_term = rule.apply(term)
-        result = narrow(new_term, goal_term, rules, bound - 1, substitions + [sub])
+        result = narrow(new_term, goal_term, rules, bound - 1, substition * sub)
         if result is not False:
             return result
     return False
 
 
 class VariantsFromRule:
-    def __init__(self, term : Term, rule : RewriteRule):
+    def __init__(self, term : Term, rule : RewriteRule, substitution : SubstituteTerm = SubstituteTerm()):
         self.current_term = term
         self.rule = rule
+        self.substitution = substitution
     def __iter__(self):
         return self
     def __next__(self):
-        return VariantsFromRule(self.rule.apply(self.current_term), self.rule)
+        sub, new_term = self.rule.apply(self.current_term)
+        return VariantsFromRule(new_term, self.rule, self.substitution * sub)
