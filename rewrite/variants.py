289c93ed1cc4d09a7bedfcf4d02bab79a51beb7a
Brandon Rozek <rozekbrandon@gmail.com>
2019-11-06 12:44:48 -0500

    narrow now returns substitute terms

--- rewrite/variants.py
+++ rewrite/variants.py
@@ -1,14 +1,14 @@
 from .rewrite import *
 
 
-def narrow(term : Term, goal_term : Term, rules : List[RewriteRule], bound : int, substitions : SubstituteTerm = []) -> List[RewriteRule]:
+def narrow(term : Term, goal_term : Term, rules : List[RewriteRule], bound : int, substitions : List[SubstituteTerm] = []) -> List[SubstituteTerm]:
     if term == goal_term:
         return substitions
     if bound < 1:
         return False
     for rule in rules:
-        new_term = rule.apply(term)[1]
-        result = narrow(new_term, goal_term, rules, bound - 1, substitions + [rule])
+        sub, new_term = rule.apply(term)
+        result = narrow(new_term, goal_term, rules, bound - 1, substitions + [sub])
         if result is not False:
             return result
     return False
