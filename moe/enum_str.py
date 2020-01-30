3328a8db3c6543b72f16d2b3d3d014d4709f0f75
Symbolic Methods <symbolicmethods@pop-os.localdomain>
2020-01-30 17:06:31 -0500

    enum_str properly enumerates strings with all possible combinations and no duplicates

--- moe/enum_str.py
+++ moe/enum_str.py
@@ -2,26 +2,60 @@
 #!/usr/bin/env python3
 from copy import deepcopy
 
-def enum_str(D: int, s: list, sig: set):
+def enum_str(D, s, sig):
 	if D <= 0:
-		temp1 = map(lambda i: "f(" + str(i) + ")", sig)
-		temp2 = map(lambda i, j: "xor(" + str(i) + "," + str(j) + ")", sig, sig)
-		for d in temp1:
-			s.append(d)
-		for d in temp2:
-			s.append(d)
+		(s1, s2) = _get_lists_for_map(list(sig)) #get two lists that have each character in sig mapped to each other character by index exactly one time
+		fList = map(lambda i: "f(" + str(i) + ")", list(sig)) #all possible f()
+		xorList = map((lambda i, j: "xor(" + str(i) + "," + str(j) + ")"), s1, s2) #all possible xor() combinations
+		for i in fList:
+			s.append(i) #add all f()
+		for j in xorList:
+			s.append(j) #add all xor()
 		return(s)
 	else:
 		rec = enum_str(D-1, s, sig)
 		patch = deepcopy(rec)
-		temp1 = map(lambda i: "f(" + str(i) + ")", list(patch))
-		temp2 = map(lambda i, j: "xor(" + str(i) + "," + str(j) + ")", list(patch), list(patch))
-		for i in temp1:
-			s.append(i)
-		for j in temp2:
-			s.append(j)
+		(p1, p2) = _get_lists_for_map(list(patch)) #get two lists that have each moe string mapped to each other moe string by index exactly one time
+		fList = map(lambda i: "f(" + str(i) + ")", list(patch)) #all possible f(x())
+		xorList = map(lambda i, j: "xor(" + str(i) + "," + str(j) + ")", p1, p2) #all possible xor(x, y()) combinations
+		for i in fList:
+			s.append(i) #add all f()
+		for j in xorList:
+			s.append(j) #add all xor()
 		return(s)
 		
+
+def _get_lists_for_map(l):
+	oldLen = len(l)
+	newLen = int((oldLen * (oldLen + 1))/2)
+	s1 = []
+	s2 = []
+	index1 = 0
+	index2 = 0
+	#create two lists to match each element with every other element with no duplicate pairs
+	#for example if l = ["a", "b", "c"] then
+	#s1 = ["a", "b", "c", "a", "b", "c"]
+	#s2 = ["a", "b", "c", "b", "c", "a"]
+	#each character is matched with each other character exactly one time
+	#this makes the map function work properly as far as I can tell
+	for x in range(newLen):
+		s1.append(l[index1])
+		s2.append(l[index2])
+		index1 += 1
+		index2 += 1
+		if(index1 >= oldLen):
+			index1 -= oldLen
+			index2 += 1
+		if(index2 >= oldLen):
+			index2 -= oldLen
+	return (s1, s2)
+
+
+
+
+
+
+
 
 s= list()
 sig = {"c", "p", "r"}
