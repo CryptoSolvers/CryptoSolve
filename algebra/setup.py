0334925552bc636b69c3135cb0e56f76788a03e2
Brandon Rozek <rozekbrandon@gmail.com>
2020-03-02 21:15:36 -0500

    Fixed versions to major.minor

--- algebra/setup.py
+++ algebra/setup.py
@@ -4,11 +4,12 @@ setup(name="algebra",
     version="0.0.1",
     packages=find_packages(),
     install_requires = [
-        "matplotlib>=3.1.1",
-        "mypy>=0.740",
-        "mypy-extensions>=0.4.0",
-        "networkx>=2.3",
-        "numpy>=1.17.0",
-        "sympy>=1.4"
+        "matplotlib~=3.1.1",
+        "mypy~=0.740",
+        "mypy-extensions~=0.4.0",
+        "networkx~=2.3",
+        "numpy~=1.17.0",
+        "sympy~=1.4",
+        "numpydoc~=0.9.2"
     ]
 )
