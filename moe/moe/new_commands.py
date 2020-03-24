3de52144c5decfd93d96b7a5eb1a8cb9c9960f4a
Symbolic Methods <symbolicmethods@pop-os.localdomain>
2020-03-24 11:35:55 -0400

    Start of random moe terminal tool feature.

---
+++ moe/moe/new_commands.py
@@ -0,0 +1,13 @@
+#import random
+from generator import MOE_Generator
+from algebra import depth, Constant, Variable
+
+
+def MOE_rand(f_depth:int=2, starts_with_IV:bool=True, chaining:bool=True):
+    c_depth = lambda t : depth(t) == f_depth
+    c_iv = lambda t : (Constant("r") in t) == starts_with_IV
+    c_chain = lambda t : (Variable("C_{i-1}") in t) == chaining
+    condition = lambda t : c_depth(t) and c_iv(t) and c_chain(t)
+    print(filter(condition, MOE_Generator()))
+    #return random.choice(filter(condition, MOE_Generator()))
+
