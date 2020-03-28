ccfbb932879189a6c482385a7c0ad29b156308b9
Symbolic Methods <symbolicmethods@pop-os.localdomain>
2020-03-28 13:30:37 -0400

    Made a command that returns an iterable of moe's with a filter for some conditions.

--- moe/moe/new_commands.py
+++ moe/moe/new_commands.py
@@ -1,13 +1,13 @@
-#import random
+import random
 from generator import MOE_Generator
 from algebra import depth, Constant, Variable
 
 
-def MOE_rand(f_depth:int=2, starts_with_IV:bool=True, chaining:bool=True):
+def generate_moe_iterable(f_depth:int=2, starts_with_IV:bool=True, chaining:bool=True):
     c_depth = lambda t : depth(t) == f_depth
     c_iv = lambda t : (Constant("r") in t) == starts_with_IV
     c_chain = lambda t : (Variable("C_{i-1}") in t) == chaining
     condition = lambda t : c_depth(t) and c_iv(t) and c_chain(t)
-    print(filter(condition, MOE_Generator()))
-    #return random.choice(filter(condition, MOE_Generator()))
+    #print(filter(condition, MOE_Generator()))
+    return iter(filter(condition, MOE_Generator()))
 
