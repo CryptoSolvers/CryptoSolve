50bffd0985b1772eb98ab31d4fe43d46e3bc205d
Symbolic Methods <symbolicmethods@pop-os.localdomain>
2020-04-02 14:25:06 -0400

    Conditional random MOE generator supports f_depth, chaining, and IV conditions.

--- moe/moe/new_commands.py
+++ moe/moe/new_commands.py
@@ -3,11 +3,13 @@ from generator import MOE_Generator
 from algebra import depth, Constant, Variable
 
 
-def generate_moe_iterable(f_depth:int=2, starts_with_IV:bool=True, chaining:bool=True):
-    c_depth = lambda t : depth(t) == f_depth
-    c_iv = lambda t : (Constant("r") in t) == starts_with_IV
-    c_chain = lambda t : (Variable("C_{i-1}") in t) == chaining
-    condition = lambda t : c_depth(t) and c_iv(t) and c_chain(t)
+def get_rand_moe_generator(f_depth:int=2, starts_with_IV:bool=True, chaining:bool=True):
+    c_depth = lambda t : depth(t) <= f_depth
+    c_iv = lambda t : (Constant("r") in t) if starts_with_IV else True
+    c_chain1 = lambda t : (Variable("C_{i-1}") in t) if chaining else True
+    c_chain2 = lambda t : (Variable("C_{i-2}") in t) if chaining else True
+    c_chain3 = lambda t : (Variable("C_{i-3}") in t) if chaining else True
+    condition = lambda t : c_depth(t) and c_iv(t) and (c_chain1(t) or c_chain2(t) or c_chain3(t))
     #print(filter(condition, MOE_Generator()))
     return iter(filter(condition, MOE_Generator()))
 
