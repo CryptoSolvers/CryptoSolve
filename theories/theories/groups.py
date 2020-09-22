7cbb6e39a046150d019feedfe1a3f562042ede62
Brandon Rozek <rozekbrandon@gmail.com>
2020-09-21 21:35:45 -0400

    Cleaned up theory module code, moved nat to theory module

--- theories/theories/groups.py
+++ theories/theories/groups.py
@@ -1,10 +1,24 @@
-from rewrite import *
-from algebra import *
-from copy import deepcopy
+"""
+An implementation of Algebraic Groups. This module can use some rework
+to utilize the sort attribute in the algebra module.
+
+See nat.py for a more modern take of implementing a theory.
+"""
+from typing import Set
+from algebra import Constant, Function, Term, Variable
+from rewrite import RewriteRule, RewriteSystem, normal
+
+__all__ = ['Group']
 
 class Group:
+    """
+    A group is a set G with an operation op that contain the
+    closure and associative properties, as well as contains
+    an identity and inverse element.
+    """
     @staticmethod
-    def create_rules(op : Function, inverse : Function, identity : Constant) -> Set[RewriteRule]:
+    def create_rules(op: Function, inverse: Function, identity: Constant) -> Set[RewriteRule]:
+        """Create the set of rewrite rules that characterizes an algebraic group."""
         x = Variable("x") ; y = Variable("y") ; z = Variable("z")
         # From page 184 of Term Rewriting and All That
         return {
@@ -33,14 +47,13 @@ class Group:
             # i(x) * (x * y) → y
             RewriteRule(op(inverse(x), op(x, y)), y)
         }
-    
-    def __init__(self, op : Function, inverse : Function, identity : Constant):
+
+    def __init__(self, op: Function, inverse: Function, identity: Constant):
         self.op = op
         self.inverse = inverse
         self.identity = identity
         self.rewrite_rules = Group.create_rules(op, inverse, identity)
-    
-    def normal(self, element : Term):
-        return normal(element, self.rewrite_rules)
 
-
+    def normal(self, element: Term):
+        """Normal form of a group term."""
+        return normal(element, RewriteSystem(self.rewrite_rules))
