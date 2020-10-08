97badcdd25a49c35ed90492446cc92f59aa5f812
Luis <luiserovira@gmail.com>
2020-10-08 19:38:46 -0400

    Created PreFilteredMOOGenerator in pre_filtered_generator.py that preemptively filters out MOOs with f depths larger than specified.

---
+++ examples/moo/pre_filtered_generator_ex.py
@@ -0,0 +1,24 @@
+"""
+Example usage of the MOOGenerator.
+"""
+from moe.pre_filtered_generator import PreFilteredMOOGenerator
+from moe.custom import CustomMOO
+from moe.check import moo_check
+from Unification.p_unif import p_unif
+
+
+g = PreFilteredMOOGenerator()
+
+# Get the 17th random term
+t = [next(g) for i in range(17)][-1]
+
+# Register the custom MOO
+tm = CustomMOO(t)
+
+# Check the mode of operation
+moo_check(tm.name, 'every', p_unif, 5)
+
+print("We can also generate more random MOOs if we'd like")
+print("The next 5 random MOO:")
+for _ in range(5):
+    print(next(g))
