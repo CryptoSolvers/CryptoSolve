ead25e977215c5b1efb2fadea9651bb2a141cde6
Waxillium <rkauffma@mail.umw.edu>
2020-10-13 21:11:25 -0400

    Mapped gui inputs to acceptable tool inputs, gui tool tab works now!

--- moe/moe/gui/moo_client.py
+++ moe/moe/gui/moo_client.py
@@ -1,146 +1,158 @@
 #gui prototype
-#pyinstaller -wF moo_client.py to create exe
+#pyinstaller moo_client.spec to create exe
 
 from moe.check import moo_check
-#from moe.website.utils import format_substitutions
+from Unification.unif import unif
+from Unification.p_syntactic import p_syntactic
+from Unification.ac_unif import ac_unify
+from Unification.p_unif import p_unif
+from Unification.xor_rooted_unif import XOR_rooted_security
 import PySimpleGUI as sg
+import operator
 
 unification_algorithms = ['Syntactic', 'F-Rooted P-XOR', 'XOR-Rooted P-XOR']
-chaining_functions = ['Cipher Block Chaining', 'Propogating Cipher Block Chaining', 'Hash Cipher Block Chaining', 'Cipher Feedback', 'Output Feedback']
+unif_dict = {'Syntactic': p_syntactic, 'F-Rooted P-XOR': p_unif, 'XOR-Rooted P-XOR': XOR_rooted_security}
+chaining_functions = ['Cipher Block Chaining', 'Propogating Cipher Block Chaining', \
+                      'Hash Cipher Block Chaining', 'Cipher Feedback', 'Output Feedback']
+cf_dict = {'Cipher Block Chaining': 'cipher_block_chaining',
+            'Propogating Cipher Block Chaining': 'propogating_cbc',
+            'Hash Cipher Block Chaining': 'hash_cbc',
+            'Cipher Feedback': 'cipher_feedback',
+            'Output Feedback': 'output_feedback'}
 schedules = ['Every', 'End']
+scd_dict = {'Every': 'every', 'End': 'end'}
 
 # returns the window that will be the client gui
 # contains all of the formatting and other cool stuff that makes the gui window
 def make_window():
     # theme properties for a theme that uses UMW colors
     sg.LOOK_AND_FEEL_TABLE['UMW'] = {'BACKGROUND': 'white',
-                                     'TEXT': '#002a5a',
-                                     'INPUT': 'white',
-                                     'TEXT_INPUT': '#000000',
-                                     'SCROLL': '#898c91',
-                                     'BUTTON': ('white', '#002a5a'),
-                                     'PROGRESS': ('#01826B', '#D0D0D0'),
-                                     'BORDER': 1, 'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0,}
+            'TEXT': '#002a5a',
+            'INPUT': 'white',
+            'TEXT_INPUT': '#000000',
+            'SCROLL': '#898c91',
+            'BUTTON': ('white', '#002a5a'),
+            'PROGRESS': ('#01826B', '#D0D0D0'),
+            'BORDER': 1, 'SLIDER_DEPTH': 0, 'PROGRESS_DEPTH': 0,}
 
     sg.theme('UMW')
 
     # size of all input boxes
     box_size = (30, 1) #for all input boxes
-    ml_box_size = (70, 1) #for output boxes
-
+    ml_box_size = (70, 15) #for output boxes
 
     #------------------------tool page layout------------------------
-    m = [sg.Multiline('', size=ml_box_size, pad=((10, 0),(110, 10)), key='-O1-')]#output box1
+    m = [sg.Multiline('', size=add_t(ml_box_size, (0, 7)), pad=((10, 0),(10, 10)), key='-O1-')]#output box1
     output = [sg.Frame('Output', [m], pad=(10, 10))]
 
     # left hand side input titles
     t_left_column = [[sg.Text('Unification Algorithm:')],
-                     [sg.Text('Chaining Function:')],
-                     [sg.Text('Schedule:')],
-                     [sg.Text('Session Length Bound:')],
-                     [sg.Text('Adversary knows IV?')]]
+            [sg.Text('Chaining Function:')],
+            [sg.Text('Schedule:')],
+            [sg.Text('Session Length Bound:')],
+            [sg.Text('Adversary knows IV?')]]
 
     # right hand side input boxes
     t_right_column = [[sg.InputCombo((unification_algorithms), size=box_size)],
-                      [sg.InputCombo((chaining_functions), size=box_size)],
-                      [sg.InputCombo((schedules), size=box_size)],
-                      [sg.InputText('10', size=box_size)],
-                      [sg.Checkbox('')]]
+            [sg.InputCombo((chaining_functions), size=box_size)],
+            [sg.InputCombo((schedules), size=box_size)],
+            [sg.InputText('10', size=box_size)],
+            [sg.Checkbox('')]]
 
     # overall layout of the tab with both input titles and boxes, execute, and output box
     tool_layout = [[sg.Frame('Settings', [[
-                    sg.Column(t_left_column),
-                    sg.Column(t_right_column, key='-TOOL-INPUT-'),
-                    sg.Column([], pad=(56,0))]],
-                    pad=(11,10))],
-                   [sg.Button('Execute!', pad=(10,0))],
-                    output]
+        sg.Column(t_left_column),
+        sg.Column(t_right_column, key='-TOOL-INPUT-'),
+        sg.Column([], pad=(56,0))]],
+        pad=(11,10))],
+        [sg.Button('Execute!', pad=(10,0))],
+        output]
 
 
     #---------------------simulation page layout---------------------
-    m = [sg.Multiline('', size=ml_box_size, pad=((10, 0),(190, 10)), key='-O2-')]
+    m = [sg.Multiline('', size=add_t(ml_box_size, (0, 12)), pad=((10, 0),(10, 10)), key='-O2-')]
     output = [sg.Frame('Output', [m], pad=(10, 10))]
 
     # left hand side input titles
     s_left_column = [[sg.Text('Chaining Function:')],
-                     [sg.Text('Schedule:')]]
+            [sg.Text('Schedule:')]]
 
     # right hand side input boxes
     s_right_column = [[sg.InputCombo((chaining_functions), size=box_size)],
-                      [sg.InputCombo((schedules), size=box_size)]]
+            [sg.InputCombo((schedules), size=box_size)]]
 
     # overall layout of the tab with both input titles and boxes, execute, and output box
     simulation_layout = [[sg.Frame('Settings', [[
-                          sg.Column(s_left_column),
-                          sg.Column(s_right_column),
-                          sg.Column([], pad=(70,0))]],
-                          pad=(10,10))],
-                         [sg.Button('Execute!', pad=(10,0))],
-                          output]
+        sg.Column(s_left_column),
+        sg.Column(s_right_column),
+        sg.Column([], pad=(70,0))]],
+        pad=(10,10))],
+        [sg.Button('Execute!', pad=(10,0))],
+        output]
 
 
     #-----------------------custom page layout-----------------------
-    m = [sg.Multiline('', size=ml_box_size, pad=((10, 0),(110, 10)), key='-O3-')]
+    m = [sg.Multiline('', size=add_t(ml_box_size, (0, 7)), pad=((10, 0),(10, 10)), key='-O3-')]
     output = [sg.Frame('Output', [m], pad=(10, 10))]
 
     # left hand side input titles
     c_left_column = [[sg.Text('Custom MOO:')],
-                     [sg.Text('Unification Algorithm:')],
-                     [sg.Text('Schedule:')],
-                     [sg.Text('Session Length Bound:')],
-                     [sg.Text('Adversary knows IV?')]]
+            [sg.Text('Unification Algorithm:')],
+            [sg.Text('Schedule:')],
+            [sg.Text('Session Length Bound:')],
+            [sg.Text('Adversary knows IV?')]]
 
     # right hand side input boxes
     c_right_column = [[sg.InputText(('f(xor(P[i],C[i-1]))'), size=box_size)],
-                      [sg.InputCombo((unification_algorithms), size=box_size)],
-                      [sg.InputCombo((schedules), size=box_size)],
-                      [sg.InputText(('10'), size=box_size)],
-                      [sg.Checkbox('')]]
+            [sg.InputCombo((unification_algorithms), size=box_size)],
+            [sg.InputCombo((schedules), size=box_size)],
+            [sg.InputText(('10'), size=box_size)],
+            [sg.Checkbox('')]]
 
     # overall layout of the tab with both input titles and boxes, execute, and output box
     custom_layout = [[sg.Frame('Settings', [[
-                      sg.Column(c_left_column),
-                      sg.Column(c_right_column),
-                      sg.Column([], pad=(56,0))]],
-                      pad=(10,10))],
-                     [sg.Button('Execute!', pad=(10, 0))],
-                      output]
+        sg.Column(c_left_column),
+        sg.Column(c_right_column),
+        sg.Column([], pad=(56,0))]],
+        pad=(10,10))],
+        [sg.Button('Execute!', pad=(10, 0))],
+        output]
 
 
     #----------------------random page layout-----------------------
-    m = [sg.Multiline('', size=ml_box_size, pad=((10, 0),(6, 10)), key='-O4-')]
+    m = [sg.Multiline('', size=ml_box_size, pad=((10, 0),(10, 10)), key='-O4-')]
     output = [sg.Frame('Output', [m], pad=(10, 10))]
 
     # left hand side input titles
     r_left_column = [[sg.Text('Chaining Required:')],
-                     [sg.Text('IV Required:')],
-                     [sg.Text('Encryption F-Bound:')],
-                     [sg.Text('Based on number of MOO to Generate:')],
-                     [sg.Text('Test Each MOO for Security:')],
-                     [sg.Text('Unification Algorithm:')],
-                     [sg.Text('Schedule:')],
-                     [sg.Text('Session Length Bound:')],
-                     [sg.Text('Adversary knows IV?')]]
+            [sg.Text('IV Required:')],
+            [sg.Text('Encryption F-Bound:')],
+            [sg.Text('Based on number of MOO to Generate:')],
+            [sg.Text('Test Each MOO for Security:')],
+            [sg.Text('Unification Algorithm:')],
+            [sg.Text('Schedule:')],
+            [sg.Text('Session Length Bound:')],
+            [sg.Text('Adversary knows IV?')]]
 
     # right hand side input boxes
     r_right_column = [[sg.InputCombo(('Yes', 'No'), size=box_size)],
-                      [sg.InputCombo(('Yes', 'No'), size=box_size)],
-                      [sg.InputText('6', size=box_size)],
-                      [sg.InputText('4', size=box_size)],
-                      [sg.InputCombo(('Yes', 'No'), size=box_size)],
-                      [sg.InputCombo((unification_algorithms), size=box_size)],
-                      [sg.InputCombo((schedules), size=box_size)],
-                      [sg.InputText('1', size=box_size)],
-                      [sg.Checkbox('')]]
+            [sg.InputCombo(('Yes', 'No'), size=box_size)],
+            [sg.InputText('6', size=box_size)],
+            [sg.InputText('4', size=box_size)],
+            [sg.InputCombo(('Yes', 'No'), size=box_size)],
+            [sg.InputCombo((unification_algorithms), size=box_size)],
+            [sg.InputCombo((schedules), size=box_size)],
+            [sg.InputText('1', size=box_size)],
+            [sg.Checkbox('')]]
 
     # overall layout of the tab with both input titles and boxes, execute, and output box
     random_layout = [[sg.Frame('Settings', [[
-                      sg.Column(r_left_column),
-                      sg.Column(r_right_column),
-                      sg.Column([], pad=(10,0))]],
-                      pad=(10,10))],
-                     [sg.Button('Execute!', pad=(10,0))],
-                      output]
+        sg.Column(r_left_column),
+        sg.Column(r_right_column),
+        sg.Column([], pad=(10,0))]],
+        pad=(10,10))],
+        [sg.Button('Execute!', pad=(10,0))],
+        output]
 
 
     #------------------------------TabGroup layout------------------------------
@@ -149,22 +161,22 @@ def make_window():
     # for each tab because of these keys
     # this allows you to tell which tab the user pressed the 'Execute!' button on
     tab_group_layout = [[sg.Tab('Tool', tool_layout, key='-TOOL-'),
-                         sg.Tab('Simulation', simulation_layout, key='-SIMULATION-'),
-                         sg.Tab('Custom', custom_layout, key='-CUSTOM-'),
-                         sg.Tab('Random', random_layout, key='-RANDOM-')]]
+        sg.Tab('Simulation', simulation_layout, key='-SIMULATION-'),
+        sg.Tab('Custom', custom_layout, key='-CUSTOM-'),
+        sg.Tab('Random', random_layout, key='-RANDOM-')]]
 
     # menu definitions
     menu_layout = [['&Help', ['&Tool', '&Simulation', '&Custom', '&Random']],
-                   ['&About', ['&README', '&Authors', '&Usage']]]
+            ['&About', ['&README', '&Authors', '&Usage']]]
 
     # The window layout - defines the entire window
     layout = [[sg.Menu(menu_layout, tearoff=True)],[sg.TabGroup(tab_group_layout,
-                tab_location='topleft',
-                tab_background_color='#002a5a',
-                selected_background_color="grey",
-                title_color='white',
-                selected_title_color='white',
-                enable_events=True,)]]
+        tab_location='topleft',
+        tab_background_color='#002a5a',
+        selected_background_color="grey",
+        title_color='white',
+        selected_title_color='white',
+        enable_events=True,)]]
 
     return sg.Window('MOO Tool', layout, no_titlebar=False)
 
@@ -243,35 +255,35 @@ def Launcher():
         # the tool yet
         if goodInput:
             if function == 'tool':
-              """
-                unif, chaining, sched, length_bound, knows_iv = result[0], result[1], result[2], result[3], result[4]
+                unif, chaining, sched, length_bound, knows_iv = \
+                unif_dict[result[0]], cf_dict[result[1]], scd_dict[result[2]], int(result[3]), result[4]
                 result = moo_check(chaining, sched, unif, length_bound, knows_iv)
                 response = ""
                 if result.secure:
-                  response = "MOO IS SECURE: "
-                  if result.syntactic_result:
-                    response += "PASSES SYNTACTIC CHECK"
-                  else:
-                    response += "NO UNIFIERS FOUND"
+                    response = "MOO IS SECURE: "
+                    if result.syntactic_result:
+                        response += "PASSES SYNTACTIC CHECK"
+                    else:
+                        response += "NO UNIFIERS FOUND"
                 else:
-                  response = "MOO IS INSECURE. COLLISIONS WITH SUBSTITUTION(S) " + \
-                    format_substitutions(result.collisions)
-                """
-              window['-O1-'].update(result)
+                    response = "MOO IS INSECURE. COLLISIONS WITH SUBSTITUTION(S) "
+                    for i in result.collisions:
+                        response += str(i) + " "
+                window['-O1-'].update(response)
             if function == 'simulation':
-              window['-O2-'].update(result)
+                window['-O2-'].update(result)
             if function == 'custom':
-              window['-O3-'].update(result)
+                window['-O3-'].update(result)
             if function == 'random':
-              window['-O4-'].update(result)
+                window['-O4-'].update(result)
 
         # menu button events create popups
         # my intent with these is for this to be a place to give users more information
         # about how to use each part of the tool, but any of this can be removed as desired
         if event == 'Tool':
             sg.Popup('Valid unification algorithm and chaining function pairs:\n\n'
-                     'Syntactic: Cipher Block Chaining, Propogating Cipher Block Chaining, \
-                     Hash Cipher Block Chaining, Cipher Feedback, Output Feedback\n'
+                     'Syntactic: Cipher Block Chaining, Propogating Cipher Block Chaining,' \
+                     ' Hash Cipher Block Chaining, Cipher Feedback, Output Feedback\n'
                      'F-Rooted P-XOR: Cipher Block Chaining, Propogating Cipher Block Chaining, Hash Cipher Block Chaining\n'
                      'XOR-Rooted P-XOR: Cipher Feedback, Output Feedback', title='Tool usage', line_width=200)
         if event == 'Simulation':
@@ -292,8 +304,12 @@ def valid_moo_unif_pair(unif_choice: str, cf_choice: str) -> bool:
         supported_chaining = ['Cipher Block Chaining', 'Propogating Cipher Block Chaining', 'Hash Cipher Block Chaining']
     # xor rooted
     else:
-        supported_chaining = ['Cipher Feedback', 'Output Feedback']
+	    supported_chaining = ['Cipher Feedback', 'Output Feedback']
     return cf_choice in supported_chaining
+
+# tuple addition
+def add_t(t1: tuple, t2: tuple) -> tuple:
+    return tuple(map(operator.add, t1, t2))
 
 if __name__ == '__main__':
     Launcher()
