d6c2dac3e9647102f229606c0c3cf7c0e489c2d4
Brandon Rozek <rozekbrandon@gmail.com>
2020-10-17 01:32:11 -0400

    Added basic arithmetic to the nat module. Should consider creating some sort of abstraction to handle function handles and rewrite rules in one.

--- theories/theories/nat.py
+++ theories/theories/nat.py
@@ -13,14 +13,44 @@ __all__ = ['zero', 'S', 'dec', 'nat_sort']
 # Constants and Functions
 nat_sort = Sort("nat")
 zero = Constant("0", sort=nat_sort)
+
+# Uniary Functions
 S = Function("S", 1, domain_sort=nat_sort, range_sort=nat_sort)
 dec = Function("dec", 1, domain_sort=nat_sort, range_sort=nat_sort)
 
+# Binary Functions
+plus = Function("plus", 2, domain_sort=nat_sort, range_sort=nat_sort)
+minus = Function("minus", 2, domain_sort=nat_sort, range_sort=nat_sort)
+mult = Function("mult", 2, domain_sort=nat_sort, range_sort=nat_sort)
+exp = Function("exp", 2, domain_sort=nat_sort, range_sort=nat_sort)
+
 # Rules
 _n = Variable("n", sort=nat_sort)
+_m = Variable("m", sort=nat_sort)
+
 dec_rule1 = RewriteRule(dec(S(_n)), _n)
 dec_rule2 = RewriteRule(dec(zero), zero)
-rules = RewriteSystem({dec_rule1, dec_rule2})
+
+plus_rule1 = RewriteRule(plus(zero, _n), _n)
+plus_rule2 = RewriteRule(plus(S(_n), _m), S(plus(_n, _m)))
+
+minus_rule1 = RewriteRule(minus(zero, _n), zero)
+minus_rule2 = RewriteRule(minus(_n, zero), _n)
+minus_rule3 = RewriteRule(minus(S(_n), S(_m)), minus(_n, _m))
+
+mult_rule1 = RewriteRule(mult(zero, _m), zero)
+mult_rule2 = RewriteRule(mult(S(_n), _m), plus(_m, mult(_n, _m)))
+
+exp_rule1 = RewriteRule(exp(_n, zero), S(zero))
+exp_rule2 = RewriteRule(exp(_n, S(_m)), mult(_n, exp(_n, _m)))
+
+rules = RewriteSystem({
+    dec_rule1, dec_rule2,
+    plus_rule1, plus_rule2,
+    minus_rule1, minus_rule2, minus_rule3,
+    mult_rule1, mult_rule2,
+    exp_rule1, exp_rule2
+})
 
 def from_int(x: int) -> Term:
     """Converts an integer to a nat."""
