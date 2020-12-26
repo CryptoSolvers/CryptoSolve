63d761cbbcaa3da695317fe9a02650d38e47df0c
Brandon Rozek <rozekbrandon@gmail.com>
2020-12-26 12:09:28 -0500

    Added READMEs to python packages that have them already

--- moe/setup.py
+++ moe/setup.py
@@ -10,10 +10,14 @@ for directory in directories:
     files = list(filter(lambda x: x + "/" not in directories, glob(directory + "*")))
     data_files.append((directory, files))
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(name="symcollab-moe",
     version="0.1.1",
     packages=find_namespace_packages(include=["symcollab.*"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/symcollab/cryptosolve",
     scripts=["moe_tool", "moe_website"],
     install_requires = [
--- rewrite/setup.py
+++ rewrite/setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_namespace_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(name="symcollab-rewrite",
     version="0.1.1",
     packages=find_namespace_packages(include=["symcollab.*"]),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/symcollab/cryptosolve",
     install_requires = [
         # Our dependencies
