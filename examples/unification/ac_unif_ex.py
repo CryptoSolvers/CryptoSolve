a4817eff23dc07b96a0d2ac1e9e3693b4f866f2d
Andrew Marshall <marshall@umw.edu>
2021-03-02 16:31:20 -0500

    ac unif work

--- examples/unification/ac_unif_ex.py
+++ examples/unification/ac_unif_ex.py
@@ -37,3 +37,22 @@ U3.add(e3)
 ac_unify(U3)
 
 
+
+from symcollab.algebra import Constant, Function, Variable, Equation
+from symcollab.Unification.ac_unif import ac_unify
+
+f = Function("f", 2)
+g = Function("g", 1)
+x = Variable("x")
+y = Variable("y")
+z = Variable("z")
+a = Constant("a")
+b = Constant("b")
+lhs = f(x, y)
+rhs = f(z, z)
+e4 = Equation(lhs, rhs)
+U4 = set()
+U4.add(e4)
+ac_unify(U4)
+
+
