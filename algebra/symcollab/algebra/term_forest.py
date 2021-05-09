081fc4bd6a81908ee48eb83c61a4186771caceab
Raymond <Raymond1_Kauffman@Yahoo.com>
2021-05-09 02:15:38 -0400

    added a ton of helper functions, deepcopy, hash, comparison, etc

--- algebra/symcollab/algebra/term_forest.py
+++ algebra/symcollab/algebra/term_forest.py
@@ -1,6 +1,8 @@
 from typing import Tuple, Dict, List, Optional, Set
 from term import *
 from anytree import AnyNode, RenderTree
+from copy import deepcopy
+from functools import partial
 
 """
 	A forest data structure that contains trees of terms 
@@ -17,12 +19,13 @@ from anytree import AnyNode, RenderTree
 
 	Notes
 	-----
-	Because a term could be expanded forever, a max_height
-	is required in order to guarantee termination.
+	In some cases, a term could be expanded forever, 
+	thereforea max_height is required in order to 
+	guarantee termination.
 
 	Examples
 	--------
-	>>> from symcollab.algebra import Term, Function, Variable, Constant, FuncTerm, Equation
+	>>> from symcollab.algebra import *
 	>>> from symcollab.algebra import Term_Forest
 	>>> f = Function("f", 1)
 	>>> g = Function("g", 1)
@@ -53,10 +56,10 @@ class Term_Forest:
 	def __init__(self, seed: List[Equation], max_height: int):
 		self.seed = seed
 		self.max_height = max_height
-		self.history = list()
+		self.__history = list()
 		self.forest = list()
 		self.__init_terms()
-		self.tree_heights = [0] * len(self.terms)
+		self.__tree_heights = [0] * len(self.terms)
 		self.__build_forest()
 	
 	# Gets unique terms from the list of equations
@@ -70,14 +73,14 @@ class Term_Forest:
 
 	# For every unique term, recursively build a tree
 	def __build_forest(self):
-		self.index = 0
+		self.__index = 0
 		for term in self.terms:
-			if term not in self.history:
-				self.history.append(term)
+			if term not in self.__history:
+				self.__history.append(term)
 				node = AnyNode(term=term)
 				self.forest.append(node)
 				self.__build_tree(node)
-			self.index += 1
+			self.__index += 1
 
 	# Recursively generate possible list of terms to add
 	# Will not add any terms that already exist in the forest
@@ -85,15 +88,15 @@ class Term_Forest:
 		nt = node.term
 		children = self.__generate_terms(nt)
 		for t in children:
-			if t not in self.history:
-				if self.tree_heights[self.index] < self.max_height:
-					self.history.append(t)
+			if t not in self.__history:
+				if self.__tree_heights[self.__index] < self.max_height:
+					self.__history.append(t)
 					n = AnyNode(term=t, parent=node)
-					if n.depth+1 > self.tree_heights[self.index]:
-						self.tree_heights[self.index] = n.depth + 1
+					if n.depth+1 > self.__tree_heights[self.__index]:
+						self.__tree_heights[self.__index] = n.depth + 1
 					self.__build_tree(n)
 				else:
-					self.tree_heights[self.index] = self.max_height
+					self.__tree_heights[self.__index] = self.max_height
 
 	# Recursively generate every possible way to rewrite a term
 	def __generate_terms(self, nt: Term) -> List[Term]:
@@ -121,7 +124,19 @@ class Term_Forest:
 
 		return children
 
-	# display each tree in the forest
+	# display each node in each tree in the forest
+	def __repr__(self):
+		forest_str = ""
+		index = 0
+		for n in self.forest:
+			index += 1
+			for pre, _, node in RenderTree(n):
+				forest_str += ("%s%s" % (pre, node)) + "\n"
+			forest_str += "height: " + str(self.get_height(n)) + "\n"
+
+		return forest_str
+
+	# display each term in each tree in the forest
 	def __str__(self):
 		forest_str = ""
 		index = 0
@@ -133,13 +148,79 @@ class Term_Forest:
 
 		return forest_str
 
-	# take one of the root terms in the forest and return the height of that tree
+	def __deepcopy__(self, memo):
+		new_forest =  Term_Forest(deepcopy(self.seed), deepcopy(self.max_height))
+		memo[id(self)] = new_forest
+		dcopy = partial(deepcopy, memo=memo)
+		return new_forest
+
+
+	# Can compare forests via length, <, <=, >, >=, ==, !=
+	def __lt__(self, other):
+		return len(self.forest) < len(other.forest)
+
+	def __le__(self, other):
+		return self == other or self < other
+
+	def __gt__(self, other):
+		return len(self.forest) > len(other.forest)
+
+	def __ge__(self, other):
+		return self == other or self > other
+
+	# determines if two forests are the same if each contain the same trees
+	def __eq__(self, other):
+		trees = len(self.forest)
+		if trees != len(other.forest):
+			return False
+		matches = 0
+		for _t in self.forest:
+			for _o in other.forest:
+				res = Term_Forest.tree_compare(_t, _o)
+				if res == 0:
+					matches += 1
+		return matches == trees
+
+	def __ne__(self, other):
+		return not self == other
+
+	#@staticmethod
+	def contains(self, term):
+		return term in self.__history
+
+	def __hash__(self):
+		_str = ""
+		for eq in self.seed:
+			_str += str(eq)
+		print(_str)
+		return hash(_str)
+
+	# given one of the root terms in the forest and return the height of that tree
 	def get_height(self, root: AnyNode) -> int:
 		index = 0
 		for t in self.terms:
 			if root.term == t:
-				return self.tree_heights[index]
+				return self.__tree_heights[index]
 			index += 1
+
+	# returns 0 if both trees contain the same nodes
+	# returns -1 if the length of root1 is less than the length of root2
+	# returns 1 if the length of root1 is more than the length of root2
+	@staticmethod
+	def tree_compare(root1, root2) -> int:
+		nodes = Term_Forest.get_tree(root1)
+		_nodes = Term_Forest.get_tree(root2)
+		if set(nodes) == set(_nodes):
+			return 0
+		return -1 if len(nodes) < len(_nodes) else 1
+
+	# return a list of all of the nodes in a given tree
+	@staticmethod
+	def get_tree(root: AnyNode) -> List[Term]:
+		tree = list()
+		for pre, _, node in RenderTree(root):
+			tree.append(node.term)
+		return tree
 
 	# Print out a description of the attributes of the forest 
 	def describe(self) -> str:
@@ -150,6 +231,6 @@ class Term_Forest:
 		for node in self.forest:
 			_terms.append(str(node.term))
 		description += str(_terms) + "\n\n"
-		description += "The forest contains " + str(len(self.history)) + " unique terms:\n"
-		description += str(self.history) + "\n"
+		description += "The forest contains " + str(len(self.__history)) + " unique terms:\n"
+		description += str(self.__history) + "\n"
 		return description
