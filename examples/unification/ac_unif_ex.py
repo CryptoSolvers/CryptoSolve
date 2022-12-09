9317e112b4044e5f422cc7c1b534bfcf9e725dc2
Brandon Rozek <rozekbrandon@gmail.com>
2022-12-09 13:48:05 -0500

    Starting cleanup on AC unification

--- examples/unification/ac_unif_ex.py
+++ examples/unification/ac_unif_ex.py
@@ -9,60 +9,11 @@ y = Variable("y")
 z = Variable("z")
 a = Constant("a")
 b = Constant("b")
-
-t = f(x, x)
-s = f(f(y, y), x)
-
-
-v = f(y,y)
-w = f(z,z)
-e1 = Equation(s,t)
-e2 = Equation(v,w)
-U = set()
-U.add(e1)
-U.add(e2)
-U2=set()
-U2.add(e1)
-
-ac_unify(U)
-
-ac_unify(U2)
-
-
-
-from symcollab.algebra import Constant, Function, Variable, Equation
-from symcollab.Unification.ac_unif import ac_unify
-
-f = Function("f", 2)
-g = Function("g", 1)
-x = Variable("x")
-y = Variable("y")
-z = Variable("z")
-a = Constant("a")
-b = Constant("b")
 lhs = f(x, y)
 rhs = f(z, z)
 e4 = Equation(lhs, rhs)
 U4 = set()
 U4.add(e4)
 ac_unify(U4)
-
-#Example for the new ac-unification method using z3
-from symcollab.algebra import Constant, Function, Variable, Equation
-from symcollab.Unification.ac_unif2 import ac_unify2
-
-f = Function("f", 2)
-g = Function("g", 1)
-x = Variable("x")
-y = Variable("y")
-z = Variable("z")
-a = Constant("a")
-b = Constant("b")
-lhs = f(x, y)
-rhs = f(z, z)
-e4 = Equation(lhs, rhs)
-U4 = set()
-U4.add(e4)
-ac_unify2(U4)
 
 
