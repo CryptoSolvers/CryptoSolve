8a1c04119b510015b259192b684851ded60e3a11
Brandon Rozek <rozekbrandon@gmail.com>
2022-12-09 22:17:40 -0500

    Added a couple rules from Boudet 1990

--- Unification/symcollab/Unification/ac_unif.py
+++ Unification/symcollab/Unification/ac_unif.py
@@ -18,6 +18,144 @@ import numpy as np # type: ignore
 from z3 import Int, Solver
 
 from symcollab.algebra import Equation, FuncTerm, get_vars, Variable
+from symcollab.Unification.common import *
+
+
+def within_equation(t: Term, e: Equation):
+    """
+    Returns True if term t appears
+    within either the left side or right
+    side of equation e.
+    """
+    for e_part in [e.left_side, e.right_side]:
+        # Split check based on Constant/Variable and FuncTerms
+        if isinstance(e2_part, (Constant, Variable)):
+            if lhs == e2_part:
+                return True
+        if isinstance(e2_part, FuncTerm):
+            if lhs in e2_part:
+                return True
+    return False
+
+def within_equations(t: Term, equations: Set[Equation]):
+    """
+    Returns True if term t appears
+    within any of the equations provided.
+    """
+    return any(
+        within_equation(t, e) for e in equations
+    )
+
+def variable_replacement(equations, orignal_equations):
+    """
+    Source; Boudet 1990
+    Variable Replacement Rule
+
+    If x and y appear in P and either
+    y occurs in the original problem or
+    x does not occur in the original problem then
+
+    P∪{x=y} => P{x -> y}∪{x=y}
+
+    Returns original equations and sigma
+    if the rule cannot be matched.
+    """
+    matched_equation: Optional[Equation] = None
+
+    for equation in equations:
+        lhs = equation.left_side
+        rhs = equation.right_side
+
+        # Make sure both sides are variables
+        if not isinstance(lhs, Variable) or not isinstance(rhs, Variable):
+            continue
+
+        # Original problem check:
+        # Either y occurs in the OG problem or x does not occur in the OG problem
+        og_problem_check = \
+            within_equations(rhs, orignal_equations) or not within_equations(lhs, orignal_equations)
+        
+        if not og_problem_check:
+            continue
+        
+        # Make sure that both the left hand side
+        # and right hand side of the equation exist
+        # in any of the other equations.
+        loccurs = within_equations(lhs, equations - {equation})
+        roccurs = within_equations(rhs, equations - {equation})
+
+        # If both sides are found, we're proceeding with this equation
+        # for the rest of the rule.
+        if loccurs and roccurs:
+            matched_equation = equation
+            break
+    
+    # If no equations are found return early
+    if matched_equation is None:
+        return equations
+    
+
+    # Create the new substitution
+    new_sub = SubstituteTerm()
+    new_sub.add(matched_equation.left_side, matched_equation.right_side)
+
+    # Apply the new substitution to the set of equations
+    new_equations = set()
+    for equation in equations - {matched_equation}:
+        new_equations.add(Equation(
+            equation.left_side * new_sub,
+            equation.right_side * new_sub
+        ))
+    
+    # Add the matched equation to the result
+    new_equations.add(matched_equation)
+    
+    return new_equations
+
+
+def remove_rule(equations, original_equations):
+    """
+    Source: Boudet 1990
+    Remove rule:
+
+    If x not in the original problem and
+    x not in either s or P then
+    {x=s}∪P => P
+
+    Note: Removes useless equations
+    that do not satisfy the condition 4
+    of the definition of the dag solved form.
+    """
+    matched_equation = None
+    for equation in equations:
+        lhs = equation.left_side
+        rhs = equation.right_side
+
+        if not isinstance(lhs, Variable):
+            continue
+
+        # Check x \not\in V(s)
+        if isinstance(rhs, (Constant, Variable)) and lhs == rhs:
+            continue
+        if isinstance(rhs, FuncTerm) and lhs in rhs:
+            continue
+
+        # Check x \not\in V(P^0)            
+        if within_equations(lhs, original_equations):
+            continue
+        
+        # Check x \not\in V(P)
+        if within_equation(lhs, equations - {equation}):
+            continue
+        
+        matched_equation = equation
+        break
+
+    if matched_equation is None:
+        return equations
+    
+    return equations - {matched_equation}
+
 
 
 
@@ -87,30 +225,15 @@ def ac_unify(U: Set[Equation]):
 
     # Return no unifiers for a set of empty equations
     if len(U) == 0:
-        return set()
+        return False # TODO: return set()
 
-    # Remove trivial equations
-    for e in U:
-        if e.right_side == e.left_side:
-            U.remove(e)
+    U = delete_trivial(U)
 
-    #Occurs Check
-    for e in U:
-        if isinstance(e.left_side, Variable) and isinstance(e.right_side, FuncTerm):
-            if e.left_side in e.right_side:
-                print('Occurs Check')
-                return set()
-        if isinstance(e.right_side, Variable) and isinstance(e.left_side, FuncTerm):
-            if e.right_side in e.left_side:
-                print('Occurs Check')
-                return set()
-
-    #Function Clash
-    for e in U:
-        if isinstance(e.left_side, FuncTerm) and isinstance(e.right_side, FuncTerm):
-            if e.left_side.function.symbol != e.right_side.function.symbol:
-                print('Function Clash')
-                return set()
+    if occurs_check(U):
+        return False # TODO: return set()
+    
+    if function_clash(U):
+        return False # TODO: return set() 
 
     # Send the problem to the diophantine solver
     delta=convert_eq(U)
